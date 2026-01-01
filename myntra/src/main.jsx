43034364725c96edfe1c468f0d3ab9477c1753de
Vishal <vjjhajhria63@gmail.com>
2026-01-01 12:24:07 +0530

    used statehook in sorting products

--- myntra/src/main.jsx
+++ myntra/src/main.jsx
@@ -3,20 +3,30 @@ import ReactDOM from "react-dom/client";
 import Header from "../component/header";
 import Card from "../component/cards";
 import products from "../utils/data";
+import { useState } from "react";
 
 const root = document.getElementById("root");
 
 
 
 function App(){
+
+    let [arr,setArr] = useState(products);
+
+    function sortarray(){
+        arr.sort((a,b)=>a.price - b.price);
+        const brr = [...arr];
+        setArr(brr);
+    }
     return (
         <>
     
         <Header/>
-
+        
+        <button onClick={sortarray}> Sort by Price </button>
         <div className="mid">
 
-            {products.map((item,index)=><Card key={index} item={item.name} offer = {item.offer} price = {item.price} link = {item.image}/>)}
+            {arr.map((item,index)=><Card key={index} item={item.name} offer = {item.offer} price = {item.price} link = {item.image}/>)}
             
         </div>  
         
